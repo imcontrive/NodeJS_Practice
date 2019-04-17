eee7b66fca9595ffec5fc64154f6d13a84d63329
shubh <psubham94@gmail.com>
2019-04-17 12:00:44 +0530

    update users file

--- 3. fs/JS/Index.js
+++ 3. fs/JS/Index.js
@@ -31,10 +31,24 @@ console.log(userPath);
 var data = {
   name:"shubham",
   status:"Available",
-  gmail:"psubham94@gmail.com"
+  gmail:"psubham94@gmail.com",
+  mob:"8860024**8"
 }
 // create files into users directory
-fs.open(userPath + '/shubh.json', 'wx',(err,fd) => {
+// fs.open(userPath + '/shubh.json', 'wx',(err,fd) => {
+//   if(err) return console.error(err);
+//   fs.writeFile(fd, JSON.stringify(data), (err) =>  {
+//     if (err) return console.error(err);
+//     console.log('File is created successfully.');
+//   });
+//   fs.close(fd,err => {
+//     if (err) return console.error(err);
+//     console.log(fd,err);    
+//   })
+// })
+
+// update files content
+fs.open(userPath + '/shubh.json', 'r+',(err,fd) => {
   if(err) return console.error(err);
   fs.writeFile(fd, JSON.stringify(data), (err) =>  {
     if (err) return console.error(err);
