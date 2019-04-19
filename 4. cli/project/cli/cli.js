7d4402acedb4f1ba93e1045d0afd1994813bd227
shubh <psubham94@gmail.com>
2019-04-19 19:16:19 +0530

    add users

--- 4. cli/project/cli/cli.js
+++ 4. cli/project/cli/cli.js
@@ -1,6 +1,10 @@
 var readline = require('readline');
 var events = require('events');
 var e = new events.EventEmitter();
+var path = require('path');
+var baseDir = path.join(__dirname, 'users');
+var fs = require('fs');
+var chalk = require('chalk');
 
 var os = require('os');
 
@@ -48,8 +52,26 @@ function center(string){
   return line;
  }
 //  verticle space
-
+function verSpace(string){
+  var line ="";
+  var len = string.length;
+  var width = process.stdout.columns -len;
+  var minispace = width/6;
+  for(var i =0 ; i< minispace ; i++){
+    line+=" ";
+  }
+  return line;
+ }
 // objects for helpMenu 
+var helpman = {
+  "chalk":"colorizes the output",
+  "clear":"clears the terminal screen",
+  "clui":" draws command-line tables, gauges and spinners",
+  "figlet":" creates ASCII art from text",
+  "inquirer":"creates interactive command-line user interface",
+  "minimist":" parses argument options",
+  "configstore":"easily loads and saves config without you having to think about where and how"
+}
 
 e.on('exit', (val) => {
   process.exit(0);
@@ -65,8 +87,16 @@ e.on('date', () => {
 
 })
 e.on('manual', (val) => {
-    //  horizontal()
-  // process.manual(0);
+  console.log(horizontal());
+  console.log("");
+  console.log("\x1b[32m%s\x1b[0m",center("Manual"));
+  console.log("");
+
+  for(var key in helpman){
+    console.log(padding("")+ key+ space(key) + helpman[key])
+    console.log("");
+  }
+  console.log(horizontal());
 })
 e.on('stats', (val) => {
   console.log("\x1b[33m%s\x1b[0m", horizontal());
@@ -77,13 +107,13 @@ e.on('stats', (val) => {
 
   console.log( horizontal());
   console.log(" ");
-  console.log("\x1b[32m%s\x1b[0m" ,padding("hostname")+ space("hostname")+ os.type());
+  console.log("\x1b[32m%s\x1b[0m" ,padding("")+ "type"+padding(os.type()));
   console.log(" ");
-  console.log("\x1b[33m%s\x1b[0m" ,padding("model")+ space("model")+os.cpus()[0].model);
+  console.log("\x1b[33m%s\x1b[0m" ,padding("")+ 'model'+padding(os.cpus()[0].model));
   console.log(" ");
-  console.log("\x1b[32m%s\x1b[0m" ,padding("hostname")+space("hostname")+ os.hostname());
+  console.log("\x1b[32m%s\x1b[0m" ,padding("")+'hostname'+padding(os.hostname()));
   console.log(" ");
-  console.log("\x1b[33m%s\x1b[0m" ,padding("Uptime")+space("Uptime")+ os.uptime().toFixed(2)/3600 +' '+ 'hours');
+  console.log("\x1b[33m%s\x1b[0m" ,padding("")+"Uptime"+ padding(os.uptime().toFixed(2)/3600 +' '+ 'hours'));
   console.log(" ");
   console.log( horizontal());
 })
@@ -92,6 +122,38 @@ e.on('single user', (val) => {
   console.log(val);
 })
 
+// multiple users
+e.on('list users', (str) => {
+
+	horizontal("");
+  console.log("");
+  console.log("");
+	horizontal("");
+	fs.readdir(baseDir, (err, userDatas) => {
+		if (!err) {
+      
+			userDatas.forEach((userData) => {
+				fs.readFile(baseDir + '/' + userData, (err, data) => {
+					if (!err) {
+            horizontal("");
+            let userFile = '';
+            // console.log(data,"datattatattatattat")
+						let stringifyData = data.toString();
+            let parsedData = JSON.parse(stringifyData)
+            // console.log(padding(parsedData));
+						userFile = `${chalk.hex('#CD5156')('Name')}: ${chalk.hex('#BB6A48')(parsedData.name)} ${chalk.hex('#CD5156')('username')}: ${chalk.hex('#BB6A48')(parsedData.username)} ${chalk.hex('#CD5156')('age')}: ${chalk.hex('#BB6A48')(parsedData.age)} ${chalk.hex('#CD5156')('rollNo')}: ${chalk.hex('#BB6A48')(parsedData.rollNo)}`;
+            console.log(center(" " + userFile));
+            console.log(" ");
+					} else {
+						console.log('Could not read the file', err);
+					}
+				})
+			})
+		} else {
+			console.log('Could not read the file', err);
+		}
+	})
+})
 
 var cli = {};
 
@@ -119,9 +181,7 @@ cli.init = () => {
   });
   rl.on('line', (input) => {
     cli.processInput(input);
-    // console.log(input);
   })
   rl.prompt();
 };
-
 module.exports = cli;
